419b29a49d84b44e5d428ef018e7e45284cc9849
Greg Burd <gburd@symas.com>
2023-09-20 19:18:43 -0400

    shrug

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         else "pre${builtins.substring 0 8 (self.lastModifiedDate or self.lastModified or "19700101")}_${self.shortRev or "dirty"}";
 
       #supportedSystems = [ "i386-linux" "x86_64-linux" "armv6l-linux" "armv7l-linux" "aarch64-linux" "powerpc64le-linux" "riscv64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" "x86_64-windows" ];
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" "x86_64-windows" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       # Generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
@@ -44,14 +44,14 @@
             inherit system;
 
             overlays = [
-              zicross.overlays.zig
-              zicross.overlays.debian
-              zicross.overlays.windows
+              #zicross.overlays.zig
+              #zicross.overlays.debian
+              #zicross.overlays.windows
 
               (final: prev: {
-                hello = with final; (if lib.strings.hasSuffix "windows" system then zigStdenv else stdenv).mkDerivation rec {
-                  inherit version;
-                  inherit system;
+                #hello = with final; (if lib.strings.hasSuffix "windows" system then zigStdenv else stdenv).mkDerivation rec {
+                hello = with final; stdenv.mkDerivation rec {
+                  inherit version system;
                   pname = "hello";
                   src = self;
                   configureFlags = [
@@ -98,14 +98,14 @@
           };
           packages.default = self.packages.${system}.hello;
 
-          packages.lxc = pkgs.callPackage ./nix/lxc.nix { package = packages.default; };
-          packages.x86_64-windows = pkgs.callPackage ./nix/pkg-win64zip.nix { package = packages.default; };
+          #packages.lxc = pkgs.callPackage ./nix/lxc.nix { package = packages.default; };
+          #packages.x86_64-windows = pkgs.callPackage ./nix/pkg-win64zip.nix { package = packages.default; };
           #packages.x86_64-wix = pkgs.callPackage ./nix/pkg-win64-wix.nix { inherit pkgs packages; };
 
           #apps.hello = flake-utils.lib.mkApp { drv = packages.default; };
           #apps.${system}.default = apps.hello;
 
-          devShells.default = import ./shell.nix { inherit pkgs; };
+          #devShells.default = import ./shell.nix { inherit pkgs; };
         }
       );
 }
